2cc9fcbc6776efe0c6c6c5840bcbcd3f2e455061
blueskyk <1419293032@qq.com>
2020-11-26 20:38:04 +0800

    分类的删除

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -10,7 +10,12 @@
             type="primary"
             size="small"
             @click="$router.push(`/categories/edit/${scope.row._id}`)"
-            >编辑</el-button
+            >编辑</el-button>
+             <el-button
+            type="danger"
+            size="small"
+            @click="remove(scope.row)"
+            >刪除</el-button
           >
         </template>
       </el-table-column>
@@ -30,10 +35,28 @@ export default {
       const res = await this.$http.get("categories");
       this.items = res.data;
     },
-  },
+   async remove(row) {
+ this.$confirm(`是否确定要删除分类 "${row.name}"`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then( async () => {
+            const res = await this.$http.delete(`categories/${row._id}`)
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+          this.fetch()
+        }).catch(() => {
+            this.$message({
+                message: "取消删除"
+            })
+        })
+   }
+    },
   created() {
     this.fetch();
-  },
+  }
 };
 </script>
 
