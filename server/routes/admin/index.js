f449cb0d2d525391917b07d270c95116e0f92e42
blueskyk <1419293032@qq.com>
2020-11-26 17:32:21 +0800

    创建分类列表

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,8 +3,14 @@ module.exports = app => {
     const router = express.Router()
     const Category = require('../../models/Category')
     router.post('/categories', async (req, res) => {
-    const model = await Category.create(req.body)
-    res.send(model)
+        const model = await Category.create(req.body)
+        res.send(model)
     })
+
+    router.get('/categories', async (req, res) => {
+        const items = await Category.find().limit(10)
+        res.send(items)
+    })
+
     app.use('/admin/api', router)
 }
